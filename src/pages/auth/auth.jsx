bc801c97e7db82e920451ab07dccf9f7e81dd8eb
ParitVr <97943731+ParitVr@users.noreply.github.com>
2023-12-21 18:54:33 +0700

    add table

---
+++ src/pages/auth/auth.jsx
@@ -0,0 +1,5 @@
+export const Auth = () => {
+    return (
+        <p>Login Page</p>
+    )
+}
--- src/pages/read-tracker/index.jsx
+++ src/pages/read-tracker/index.jsx
@@ -1,8 +1,26 @@
 export const ReadTracker = () => {
     return (
-        <div>
-            <p>Hello world</p>
-            <p>Hello world</p>
+        <div className="container">
+            <div className="card">
+                <div className="card-body">
+                    <table className="table table-responsive">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col" >Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Shadow</td>
+                                <td>Reading</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>      
         </div>
     )
 }
